786c19a162d06081b02552b127d52bd3a6b769be
pilpan <plip2010195@gmail.com>
2022-09-27 15:36:44 +0300

    done routes

--- name-of-app/.eslintrc.cjs
+++ name-of-app/.eslintrc.cjs
@@ -1,6 +1,18 @@
+/* eslint-disable no-undef */
 module.exports = {
     extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
     parser: '@typescript-eslint/parser',
+    rules: {
+        "@typescript-eslint/explicit-module-boundary-types": "off"
+      },
+      overrides: [
+        {
+          files: ["*.ts", "*.mts", "*.cts", "*.tsx"],
+          rules: {
+            "@typescript-eslint/explicit-module-boundary-types": "error"
+          }
+        }
+      ],
     plugins: ['@typescript-eslint'],
     root: true,
   };
---
+++ name-of-app/src/components/Reg/Reg.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const Reg:React.FC =() => {
+  return (
+    <div>Reg</div>
+  )
+}
