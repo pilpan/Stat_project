b577e9bc2315816007446d52171df5a73505112e
pilpan <plip2010195@gmail.com>
2022-09-27 16:31:56 +0300

    done reg page

--- name-of-app/src/components/Reg/Reg.tsx
+++ name-of-app/src/components/Reg/Reg.tsx
@@ -1,7 +1,34 @@
 import React from 'react'
 
 export const Reg:React.FC =() => {
+  const subHandler  = (e: React.SyntheticEvent) => {
+    e.preventDefault();
+    const target = e.target as typeof e.target & {
+      name: { value: string };
+      email: { value: string };
+      password: { value: string };
+    };
+    console.log({name: target.name.value, email: target.email.value, password: target.password.value});
+  }
   return (
-    <div>Reg</div>
+    <div className='container'>
+      <form onSubmit={subHandler}>
+    <label>
+    Name:
+    <input type="text" name="name" />
+  </label>
+    <label>
+    Email:
+    <input type="text" name="email" />
+  </label>
+    <label>
+    Password:
+    <input type="password" name="password" />
+  </label>
+    <button type="submit" value="Submit">
+    Sumbit
+    </button>
+    </form>
+    </div>
   )
 }
